d21000f2fad4417570bf5661057312223ec0be25
Michał K <k.michal@zoho.com>
2017-04-08 14:59:14 +0200

    Initial implementation of class Grid.

---
+++ src/grid.cpp
@@ -0,0 +1,45 @@
+#include "grid.h"
+
+Grid::Grid(int rows, int cols, QObject *parent)
+    : QObject(parent)
+{
+    appendRows(rows);
+    appendCols(cols);
+}
+
+void Grid::appendRows(int n)
+{
+    setSize(rows() + n, cols());
+}
+
+void Grid::appendCols(int n)
+{
+    setSize(rows(), cols() + n);
+}
+
+void Grid::setSize(int rows, int cols)
+{
+    const int currentRows = this->rows();
+
+    if (rows > currentRows)
+        m_grid.insert(m_grid.end(), rows - currentRows, QVector<Cell>(cols));
+    if (rows < currentRows)
+        m_grid.erase(m_grid.end() - (currentRows - rows), m_grid.end());
+    for (auto &row : m_grid) {
+        if (cols > row.size())
+            row.insert(row.end(), cols - row.size(), Cell());
+        else if (cols < row.size())
+            row.erase(row.end() - (row.size() - cols), row.end());
+    }
+
+    emit sizeChanged(rows, cols);
+}
+
+void Grid::setCellStateAt(int x, int y, bool state)
+{
+    if (m_grid[x][y].m_state == state)
+        return;
+
+    m_grid[x][y].m_state = state;
+    emit cellStateChanged(x, y, state);
+}
---
+++ src/grid.h
@@ -0,0 +1,46 @@
+#ifndef GRID_H_INCLUDED
+#define GRID_H_INCLUDED
+
+#include <QObject>
+#include <QVariant>
+#include <QVector>
+
+class Grid : public QObject
+{
+    Q_OBJECT
+public:
+    class Cell
+    {
+    public:
+        QVariant& data() const { return m_data; }
+
+    private:
+        friend class Grid;
+
+        mutable QVariant m_data;
+        bool m_state = false;
+    };
+
+    Grid(int rows, int cols, QObject *parent = nullptr);
+
+public slots:
+    void appendRows(int n);
+    void appendCols(int n);
+    void setSize(int rows, int cols);
+
+    void setCellStateAt(int x, int y, bool state);
+
+signals:
+    void sizeChanged(int newSizeX, int newSizeY);
+    void cellStateChanged(int x, int y, bool state);
+
+public:
+    const Cell& cellAt(int x, int y) const { return m_grid[x][y]; }
+    int rows() const { return m_grid.size(); }
+    int cols() const { return m_grid[0].size(); }
+
+private:
+    QVector<QVector<Cell>> m_grid;
+};
+
+#endif /* GRID_H_INCLUDED */
